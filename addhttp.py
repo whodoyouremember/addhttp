f0130b2b422bdb52c7317013534cb4e365eddfe3
whodoyouremember <37097718+whodoyouremember@users.noreply.github.com>
2019-05-14 10:18:59 +0800

    UPDATE
    solve a bug.

--- addhttp.py
+++ addhttp.py
@@ -2,45 +2,43 @@ import requests
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from urlparse import *
+from urllib.parse import urlparse
 
 
-# 若原域名有http或者https则去掉只保留域名
-def http_face(url):  
+def http_face(url):
 	if 'http' in url:
 		url= urlparse(url)
 		return url.netloc
 	else:
 		return url
 
-# 给域名添加http：//并判断是否连接成功
 def req(url):
 	url = "http://" + url
-	for i in range(3):
-		try:
-			url_ = requests.get(url, timeout=3)
-			code = url_.status_code
-			if code == 200 :
-				return url
-			else:
-				return False
-		except :
-			pass
-		sleep(0.8)
-	return False
+	try:
+		url_ = requests.get(url, timeout=3)
+		code = url_.status_code
+		if code == 200 :
+			return url
+		else:
+			return False
+	except :
+		pass
 
-# 访问url，获取最终url
+
 def real_url(url):
-	chrome_options=Options()
-	chrome_options.add_argument('--headless')
-	browser = webdriver.Chrome(options=chrome_options)
-	browser.get(url)
-	sleep(1)
-	url = browser.current_url
-	return url
-	browser.close()
-	browser.quit()
-
+	try:
+		chrome_options=Options()
+		chrome_options.add_argument('--headless')
+		browser = webdriver.Chrome(options=chrome_options)
+		browser.get(url)
+		sleep(1)
+		url = browser.current_url
+	except:
+		pass
+	finally:
+		browser.close()
+		browser.quit()		
+		return url
 
 f = open('test.txt') 
 
@@ -50,8 +48,8 @@ for url in f:
 	url = req(url)
 	if url != False:
 		url = real_url(url)
-		print url
-		with open("test.csv","a") as mon:
+		print (url)
+		with open("url.csv","a") as mon:
 			mon.write(url + '\n')
 
-print 11111	
+print 'Finish!!!'
